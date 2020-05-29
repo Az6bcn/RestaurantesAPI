ce31c78c4419596f68b9d89d8781e179449c08c1
Azeez O. Odu <az6bcn@gmail.com>
2020-05-29 18:27:01 +0100

    Architecture struture
    add projects and folders needed

---
+++ Restaurants.DataAccess/Context/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Restaurants.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Restaurants.Tests/Business.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Restaurants.Tests
+{
+    public class Class1
+    {
+    }
+}
